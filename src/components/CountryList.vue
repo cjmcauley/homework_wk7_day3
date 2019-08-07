0c5322200fd54457ba57f486c83820f0158f0a56
cjmcauley <christopher.mcauley@gmail.com>
2019-08-07 17:31:30 +0100

    dropdown added

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,5 +1,10 @@
 <template lang="html">
   <div>
+    <select>
+      <option disables value="">Select a Country</option>
+      <option v-for="(country, index) in countries" :country="country" :key="index">{{country.name}}</option>
+    </select>
+
     <ul>
       <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
     </ul>
